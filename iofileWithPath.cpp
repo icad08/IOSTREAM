76c661e5d315bb769e730ea8f6c8a3a1bc0b4213
icad08 <irsyadrm08@gmail.com>
2024-06-04 14:58:05 +0700

    copas dari file sebelumnya

---
+++ iofileWithPath.cpp
@@ -0,0 +1,48 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string baris;
+  
+    //membuka fuke dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">= menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis 
+    while (true){
+        cout << "_ ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukkan karakter q
+        if(baris == "q") break;
+        //menulis dan memasukkan nilai dari baris ke dalam file
+        outfile << baris << endl;
+    }
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+    //membuka file dalam mode membaca 
+    ifstream infile;
+    //menunjuk ke sebuah file 
+    infile.open("contohfile.txt");
+
+    cout << endl << ">= membuka dan membaca file" << endl;
+    //jika file ada maka
+    if (infile.is_open()){
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris)){
+            //dan tampilkan disini
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+    }
+    //jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "unable to open file";
+    return 0;
+}
