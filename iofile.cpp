8e2c6b6e86a44a1554a58a859955e53026774f7f
icad08 <irsyadrm08@gmail.com>
2024-06-04 14:37:42 +0700

    add string and ofstream

--- iofile.cpp
+++ iofile.cpp
@@ -4,5 +4,14 @@
 using namespace std;
 
 int main(){
+    string baris;
+    //membuka fuke dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">= menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis 
     
 }
