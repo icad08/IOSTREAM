f398895976b5a4ab8e737fc4150941f1a67a6077
icad08 <irsyadrm08@gmail.com>
2024-06-04 14:38:57 +0700

    add while true

--- iofile.cpp
+++ iofile.cpp
@@ -13,5 +13,13 @@ int main(){
     cout << ">= menulis file, \'q\' untuk keluar" << endl;
 
     //unlimited loop untuk menulis 
-    
+    while (true){
+        cout << "_ ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukkan karakter q
+        if(baris == "q") break;
+        //menulis dan memasukkan nilai dari baris ke dalam file
+        outfile << baris << endl;
+    }
 }
