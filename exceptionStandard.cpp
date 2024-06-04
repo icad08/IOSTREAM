40d70063f0974213659b621259393cbc441c63c8
icad08 <irsyadrm08@gmail.com>
2024-06-04 15:09:09 +0700

    add try

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -6,5 +6,12 @@
 using namespace std;
 
 int main(){
+    cout << "awal program" << endl; // mark 1
+    try {
+        array <int, 3> data = {3, 5, 7};
+        //pesan array integer 3 element
+        cout << data.at(5) << endl;
+        //memanggil array element 5
+    }
     
 }
