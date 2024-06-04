70cd1e0ff381860d534543347b6ae4468eda5914
icad08 <irsyadrm08@gmail.com>
2024-06-04 15:06:29 +0700

    make file cpp

---
+++ exceptionStandard.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <exception>
+//untuk object exception yang akan digunakan
+#include <array>
+//untuk object array yang akan kita gunakan
+using namespace std;
+
+int main(){
+    
+}
